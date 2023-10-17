3c7d54236e254b88eb2afcb66bc7e403372b255b
bmmuc <bmmuc@cin.ufpe.br>
2023-10-17 10:17:26 -0300

    added v0 ik

--- rsoccer_gym/__init__.py
+++ rsoccer_gym/__init__.py
@@ -4,7 +4,10 @@ register(id='VSS-v0',
          entry_point='rsoccer_gym.vss.env_vss:VSSEnv',
          max_episode_steps=1200
          )
-
+register(id='VSSIK-v0',
+         entry_point='rsoccer_gym.vss.env_vss:VSSEnvIK',
+         max_episode_steps=1200
+         )
 register(id='VSSMA-v0',
          entry_point='rsoccer_gym.vss.env_ma:VSSMAEnv',
          max_episode_steps=1200
